b730701cb8364250c7f932fc0b4fc49ac42e9f94
andrew-thox <andrewkenyon123@gmail.com>
2017-04-26 13:57:43 +0100

    added description to topic pages (#20)

--- application/cms/forms.py
+++ application/cms/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class PageForm(FlaskForm):
     title = StringField(label='title', validators=[DataRequired()])
+    description = TextAreaField(label='description', validators=[DataRequired()])
