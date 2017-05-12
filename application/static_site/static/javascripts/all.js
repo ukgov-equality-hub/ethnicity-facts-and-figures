dac0daae65fd3c9c02f1e8a41a8ccf17b0729397
Akin Peter Egbewole <hi@akinegb.com>
2017-05-12 11:02:49 +0100

    copy changes

--- application/static_site/static/javascripts/all.js
+++ application/static_site/static/javascripts/all.js
@@ -1,3 +1,67 @@
+console.log('charts');
+$(document).ready(function () {
+
+  Highcharts.chart('chart', {
+    colors: ['#85AFD0'],
+    chart: {
+      type: 'bar',
+      marginBottom: true,
+      marginLeft: 160
+    },
+    title: {
+      text: ''
+    },
+    xAxis: {
+      categories: ['Indian', 'Pakistani', 'Other Asian', 'Black', 'Chinese', 'Mixed', 'White', 'Other'],
+      title: {
+        text: null
+      }
+    },
+    yAxis: {
+      min: 0,
+      max: 15,
+      title: {
+        text: 'unemployment rate (%)',
+        align: 'high'
+      },
+      labels: {
+        overflow: 'justify'
+      }
+    },
+    labels: {
+      style: {
+        fontFamily: "nta"
+      }
+    },
+    plotOptions: {
+      bar: {
+        pointWidth: 40, 
+        dataLabels: {
+          enabled: true,
+          color: '#000',
+          align: 'left',
+          style: {
+            textOutline: false,
+            fontSize: "16px",
+            fontFamily: "nta",
+            fontWeight: "400"
+          },
+          formatter: function() {return this.y + '%'},
+          inside: true,
+          rotation: 0
+        }
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{
+      name: 'unemployment rate',
+      data: [10, 10, 5, 10, 10, 10, 5, 9]
+    }]
+  });
+
+});
 // = require_tree ./govuk
 
 (function(){
@@ -33,7 +97,6 @@
       }
     })
   }
-
   $(document).ready(accordions)
 }())
 
