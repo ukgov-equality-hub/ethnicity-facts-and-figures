5bc65892bdc42ab0baedc8ed7316f69b81aa19f0
Akin Peter Egbewole <hi@akinegb.com>
2017-05-22 15:50:50 +0100

    add table markup to preview template + css/js

---
+++ src/js/table.js
@@ -0,0 +1,47 @@
+function Table(table) {
+
+  var module = this;
+  var $table = table ?  table : $("#table");
+  var $headings = $table.find('thead td'), ordering, cachedIndex;
+
+  this.ordering = function(index) {
+    var firstClick = cachedIndex !== index;
+    if(firstClick) {
+      ordering = 'desc';
+    } else {
+      ordering = ordering !== 'asc' ? 'asc' : 'desc';
+    }
+    cachedIndex = index;
+  }
+
+  if($headings.length) {
+
+	  var dataTable = $table.DataTable({
+	    "paging":   false,
+	    "searching": false,
+	    "info":     false,
+	  });
+
+	  $.each($headings, function (index) {
+	    var $button = $(this).find('button');
+	    $button.click(function () {
+	      module.ordering(index);
+	      $(this).unbind().attr('class', 'sorting_' + ordering);
+	      dataTable.order( [index,  ordering]).draw()
+	    }.bind(this))
+	  })
+	}
+
+  return module;
+
+}
+
+$(document).ready(function () {
+
+	var $tables = $(".table");
+
+	$.each($tables, function() {
+		new Table($(this));
+	});
+
+});
