ba8f6909a096e58c1e32ede032c451dbd3e1eece
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-05-25 16:45:51 +0100

    render chart objects in dimensions

--- application/static_site/views.py
+++ application/static_site/views.py
@@ -100,4 +100,5 @@ def measure_page(topic, subtopic, measure):
         if current_user.is_departmental_user():
             if measure_page.meta.status not in ['DEPARTMENT_REVIEW', 'ACCEPTED']:
                 return render_template('not_ready_for_review.html')
-        return render_template('measure.html', topic=topic, measure_page=measure_page)
+        dimensions = [d.__dict__() for d in measure_page.dimensions];
+        return render_template('measure.html', topic=topic, measure_page=measure_page, dimensions=dimensions)
