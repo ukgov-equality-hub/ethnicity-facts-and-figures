4f7ba5231be35cd4c3734852cbcaa7667de2a22e
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-05-28 12:49:31 +0100

    autogenerator generates tables for bar charts

--- tests/test_a_data_utils.py
+++ tests/test_a_data_utils.py
@@ -1,5 +1,25 @@
 import pytest
 
+from application.cms.data_utils import Autogenerator
 
-def test_stub_chart_object(stub_chart_object):
-    assert stub_chart_object['type'] == 'bar'
+
+def test_chart_processor_generates_table_for_simple_chart_object(stub_measure_page, stub_simple_chart_object):
+
+    page = stub_measure_page
+    page.dimensions[0].chart = stub_simple_chart_object
+
+    Autogenerator().autogenerate(page=page)
+
+    assert page.dimensions[0].table != ''
+    assert page.dimensions[0].table['type'] == 'simple'
+
+
+def test_chart_processor_generates_table_for_grouped_chart_object(stub_measure_page, stub_grouped_chart_object):
+
+    page = stub_measure_page
+    page.dimensions[0].chart = stub_grouped_chart_object
+
+    Autogenerator().autogenerate(page=page)
+
+    assert page.dimensions[0].table != ''
+    assert page.dimensions[0].table['type'] == 'grouped'
