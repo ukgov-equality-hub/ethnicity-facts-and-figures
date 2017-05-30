4bbfc84f4348797f7e9c63daefe4a1a3fa4846a1
Akin Peter Egbewole <hi@akinegb.com>
2017-05-30 14:57:40 +0100

    checkouted charts.js

--- application/static/javascripts/rd-graph.js
+++ application/static/javascripts/rd-graph.js
@@ -2,181 +2,131 @@
  * Created by Tom.Ridd on 05/05/2017.
  */
 
- function drawChart(container_id, chartObject) {
-  if(chartObject.type === 'bar') {
-    return barchart(container_id, chartObject);
-  } else if(chartObject.type === 'line') {
-    return linechart(container_id, chartObject);
-  } else if(chartObject.type === 'component') {
-    return componentChart(container_id, chartObject);
-  }
+function drawChart(container_id, chartObject) {
+    if(chartObject.type === 'bar') {
+        return barchart(container_id, chartObject);
+    } else if(chartObject.type === 'line') {
+        return linechart(container_id, chartObject);
+    } else if(chartObject.type === 'component') {
+        return componentChart(container_id, chartObject);
+    }
 }
 
 function barchart(container_id, chartObject) {
-  adjustChartObject(chartObject);
-  return Highcharts.chart(container_id, {
-    chart: {
-      type:'bar',
-      height: 600
-    },
-    title: {
-      text: chartObject.title.text
-    },
-    xAxis: {
-      categories: chartObject.xAxis.categories,
-      title: {
-        text: chartObject.xAxis.title.text
-      }
-    },
-    yAxis: {
-      title: {
-        text: chartObject.yAxis.title.text
-      }
-    },
-    credits: {
-      enabled: false
-    },
-    legend: {
-      enabled: (chartObject.series.length > 1)
-    },
-    plotOptions: {
-      bar: {
-        pointWidth: 45,
-        dataLabels: {
-          enabled: true,
-          color: '#000',
-          align: 'left',
-          style: {
-            textOutline: false,
-            fontSize: "16px",
-            fontFamily: "nta",
-            fontWeight: "400"
-          },
-          formatter: function() {
-            return this.y > 0.0001 ? this.y : 'Sample size too small'
-          },
-          inside: true,
-          rotation: 0
-        }
-      },
-      series: {
-        states: {
-          hover: {
+    adjustChartObject(chartObject);
+    return Highcharts.chart(container_id, {
+        chart: {
+            type:'bar'
+        },
+        title: {
+            text: chartObject.title.text
+        },
+        xAxis: {
+            categories: chartObject.xAxis.categories,
+            title: {
+                text: chartObject.xAxis.title.text
+            }
+        },
+        yAxis: {
+            title: {
+                text: chartObject.yAxis.title.text
+            }
+        },
+        credits: {
             enabled: false
-          }
-        }
-      }
-    },
-    tooltip: barChartTooltip(chartObject),
-    series: chartObject.series
-  });
-}
+        },
+        legend: {
+            enabled: (chartObject.series.length > 1)
+        },
+        tooltip: barChartTooltip(chartObject),
+        series: chartObject.series
+    });}
 
 function barChartTooltip(chartObject) {
-  if(chartObject.series.length > 1)
-  {
-    return { pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>'
-    + chartObject.number_format.prefix + '{point.y}' + chartObject.number_format.suffix + '</b><br/>', enabled: false }
-  } else {
-    return { pointFormat: '<span style="color:{point.color}">\u25CF</span><b>'
-    + chartObject.number_format.prefix + '{point.y}' + chartObject.number_format.suffix + '</b><br/>', enabled: false }
-  }
+    if(chartObject.series.length > 1)
+    {
+        return { pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>'
+        + chartObject.number_format.prefix + '{point.y}' + chartObject.number_format.suffix + '</b><br/>' }
+    } else {
+        return { pointFormat: '<span style="color:{point.color}">\u25CF</span><b>'
+        + chartObject.number_format.prefix + '{point.y}' + chartObject.number_format.suffix + '</b><br/>'}
+    }
 }
 
 function linechart(container_id, chartObject) {
-  adjustChartObject(chartObject);
-  return Highcharts.chart(container_id, {
+    adjustChartObject(chartObject);
+    return Highcharts.chart(container_id, {
 
-    title: {
-      text: chartObject.title.text
-    },
-    xAxis: {
-      categories: chartObject.xAxis.categories,
-      title: {
-        text: chartObject.xAxis.title.text
-      }
-    },
-    yAxis: {
-      title: {
-        text: chartObject.yAxis.title.text
-      }
-    },
-    tooltip: lineChartTooltip(chartObject),
-    credits: {
-      enabled: false
-    },
-    plotOptions: {
-      series: {
-        states: {
-          hover: {
+        title: {
+            text: chartObject.title.text
+        },
+        xAxis: {
+            categories: chartObject.xAxis.categories,
+            title: {
+                text: chartObject.xAxis.title.text
+            }
+        },
+        yAxis: {
+            title: {
+                text: chartObject.yAxis.title.text
+            }
+        },
+        tooltip: lineChartTooltip(chartObject),
+        credits: {
             enabled: false
-          }
-        }
-      }
-    },
-    series: chartObject.series
-  });
-}
+        },
+        series: chartObject.series
+    });}
 
 
 function lineChartTooltip(chartObject) {
-  return { pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>'
-  + chartObject.number_format.prefix + '{point.y}' + chartObject.number_format.suffix + '</b><br/>' }
+    return { pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>'
+    + chartObject.number_format.prefix + '{point.y}' + chartObject.number_format.suffix + '</b><br/>' }
 }
 
 function componentChart(container_id, chartObject) {
-  adjustChartObject(chartObject);
-  return Highcharts.chart(container_id, {
-    chart: {
-      type:'bar'
-    },
-    title: {
-      text:  chartObject.title.text
-    },
-    xAxis: {
-      categories: chartObject.xAxis.categories,
-      title: {
-        text: chartObject.xAxis.title.text
-      }
-    },
-    yAxis: {
-      title: {
-        text: chartObject.yAxis.title.text
-      }
-    },
-    legend: {
-      reversed: true
-    },
-    plotOptions: {
-      series: {
-        stacking: 'normal'
-      }
-    },
-    tooltip: barChartTooltip(chartObject),
-    credits: {
-      enabled: false
-    },
-    plotOptions: {
-      series: {
-        states: {
-          hover: {
+    adjustChartObject(chartObject);
+    return Highcharts.chart(container_id, {
+        chart: {
+            type:'bar'
+        },
+        title: {
+            text:  chartObject.title.text
+        },
+        xAxis: {
+            categories: chartObject.xAxis.categories,
+            title: {
+                text: chartObject.xAxis.title.text
+            }
+        },
+        yAxis: {
+            title: {
+                text: chartObject.yAxis.title.text
+            }
+        },
+        legend: {
+            reversed: true
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            }
+        },
+        tooltip: barChartTooltip(chartObject),
+        credits: {
             enabled: false
-          }
-        }
-      }
-    },
-    series: chartObject.series
-  });
-}
+        },
+        series: chartObject.series
+    });}
 
-function adjustChartObject(chartObject) {
-  var multiplier = chartObject.number_format.multiplier;
-  if(multiplier != 1.0) {
-    for(s in chartObject.series) {
-      for(d in chartObject.series[s].data) {
-        var value = (multiplier * chartObject.series[s].data[d]);
-        chartObject.series[s].data[d] = Math.round(value * 100)/100;
-      }
+    function adjustChartObject(chartObject) {
+        var multiplier = chartObject.number_format.multiplier;
+        if(multiplier != 1.0) {
+            for(s in chartObject.series) {
+                for(d in chartObject.series[s].data) {
+                    var value = (multiplier * chartObject.series[s].data[d]);
+                    chartObject.series[s].data[d] = Math.round(value * 100)/100;
+                }
+            }
+        }
     }
-  }
-}
