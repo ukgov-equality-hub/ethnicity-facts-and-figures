b1f18aa9daef845973d7360715bb2743e6c70e76
Andrew Kenyon <andrewkenyon123@gmail.com>
2017-06-08 11:10:59 +0100

    removed rogue print statements

--- application/static_site/views.py
+++ application/static_site/views.py
@@ -42,8 +42,6 @@ def measure_page(topic, subtopic, measure):
             if measure_page.meta.status not in ['DEPARTMENT_REVIEW', 'ACCEPTED']:
                 return render_template('static_site/not_ready_for_review.html')
         dimensions = [d.__dict__() for d in measure_page.dimensions]
-        print("RENDERING MEASURE PAGE")
-        print(dimensions[0]["table"])
         return render_template('static_site/measure.html',
                                topic=topic,
                                measure_page=measure_page,
