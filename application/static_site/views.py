7da4461dc4158731031b5bd4dcc7e7c7abee05f7
Adam Shimali <adam.shimali@gmail.com>
2017-06-08 12:51:20 +0100

    If no subtopic list, then ordering is as it comes.

--- application/static_site/views.py
+++ application/static_site/views.py
@@ -25,12 +25,14 @@ def topic(topic):
     guid = 'topic_%s' % topic.replace('-', '')
     page = page_service.get_page(guid)
     subtopics = page_service.get_subtopics(page)
-    ordered_subtopics = []
-    for st in page.subtopics:
-        for s in subtopics:
-            if s['subtopic'].meta.guid == st:
-                ordered_subtopics.append(s)
-    return render_template('static_site/topic.html', page=page, subtopics=ordered_subtopics)
+    if page.subtopics:
+        ordered_subtopics = []
+        for st in page.subtopics:
+            for s in subtopics:
+                if s['subtopic'].meta.guid == st:
+                    ordered_subtopics.append(s)
+        subtopics = ordered_subtopics
+    return render_template('static_site/topic.html', page=page, subtopics=subtopics)
 
 
 @static_site_blueprint.route('/<topic>/<subtopic>/measure/<measure>')
