5527b3539af8546ee7f6382fef5a8c836d961e25
Adam Shimali <adam.shimali@gmail.com>
2017-06-27 17:02:21 +0100

    Only click to toggle open close if accordion is not already open or closed.

--- application/static/javascripts/all.js
+++ application/static/javascripts/all.js
@@ -281,7 +281,17 @@ $(document).ready(function () {
       e.preventDefault();
       $(this).text(expanded ? 'Open all' : 'Close all');
       expanded = expanded ? false : true;
-      $headers.click();
+      $.each($headers, function(index, header){
+        if(expanded) {
+            if (!$(header).find('span').hasClass('open')) {
+                $(header).click();
+            }
+        } else {
+          if ($(header).find('span').hasClass('open')) {
+                $(header).click();
+            }
+        }
+      });
     })
   })
 
