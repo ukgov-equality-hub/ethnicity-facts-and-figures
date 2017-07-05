989e2aeb553ca577afcd25cb24aca2ed5b25f951
Akin Peter Egbewole <hi@akinegb.com>
2017-07-05 15:47:59 +0100

    print button styling + functionality

--- application/static/javascripts/all.js
+++ application/static/javascripts/all.js
@@ -103,6 +103,27 @@ $(document).ready(function () {
   $(document).ready(accordions)
 }())
 
+$(document).ready(function () {
+  var $details = $('details');
+  $('.js--print').click(function (e) {
+    var states = [];
+    e.preventDefault();
+    $.each($details, function () {
+      var flag = $(this).prop('open');
+      states.push(flag ? true : false)
+      $(this).prop('open', true);
+    });
+    window.print();
+
+    // re-apply state to details widgets
+    $(window).scroll(function () {
+      $.each($details, function(i) {
+        $(this).attr('open', states[i]);
+      });
+      $(window).unbind('scroll');
+    });
+  });
+});
 // Stageprompt 2.0.1
 //
 // See: https://github.com/alphagov/stageprompt
