9bef69cc65c6a9b4a1951a131b9b970c80710f48
Adam Shimali <adam.shimali@gmail.com>
2017-07-14 10:48:24 +0100

    Merge from master required update of migration down revision.

--- migrations/versions/e553b195fe90_.py
+++ migrations/versions/e553b195fe90_.py
@@ -1,25 +1,20 @@
 """empty message
 
-Revision ID: af17e209d722
-Revises: 5aba17d3cea1
-Create Date: 2017-07-12 18:12:48.707221
+Revision ID: e553b195fe90
+Revises: 2c6aab556fef
+Create Date: 2017-07-14 10:46:24.359860
 
 """
 import json
-
-import sqlalchemy
 from alembic import op
 import sqlalchemy as sa
-
-
-# revision identifiers, used by Alembic.
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.attributes import flag_modified
-
 from application.cms.models import DbDimension
 
-revision = 'af17e209d722'
-down_revision = '5aba17d3cea1'
+# revision identifiers, used by Alembic.
+revision = 'e553b195fe90'
+down_revision = '2c6aab556fef'
 branch_labels = None
 depends_on = None
 
@@ -34,28 +29,28 @@ def upgrade():
 
         if d.chart is not None and isinstance(d.chart, str):
             if d.chart.strip() == '':
-                d.chart = sqlalchemy.null()
+                d.chart = sa.null()
             else:
                 d.chart = json.loads(d.chart)
             modified = True
 
         if d.chart_source_data is not None and isinstance(d.chart_source_data, str):
             if d.chart_source_data.strip() == '':
-                d.chart_source_data = sqlalchemy.null()
+                d.chart_source_data = sa.null()
             else:
                 d.chart_source_data = json.loads(d.chart_source_data)
             modified = True
 
         if d.table is not None and isinstance(d.table, str):
             if d.table.strip() == '':
-                d.table = sqlalchemy.null()
+                d.table = sa.null()
             else:
                 d.table = json.loads(d.table)
             modified = True
 
         if d.table_source_data is not None and isinstance(d.table_source_data, str):
             if d.table_source_data.strip() == '':
-                d.table_source_data = sqlalchemy.null()
+                d.table_source_data = sa.null()
             else:
                 d.table_source_data = json.loads(d.table_source_data)
             modified = True
