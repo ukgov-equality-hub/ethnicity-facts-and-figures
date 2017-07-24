381afa4a644d408f3823ae0036678a1e8651243c
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-07-24 12:24:39 +0100

    test failing mocha tests

--- test/test.js
+++ test/test.js
@@ -4,5 +4,13 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
     });
+
+    it('should return 0 when the value is in the first position', function () {
+      assert.equal(0, [1,2,3].indexOf(1));
+    });
+
+    it('should return 2 when the value is in the third position', function () {
+      assert.equal(2, [1,2,3].indexOf(1));
+    });
   });
 });
