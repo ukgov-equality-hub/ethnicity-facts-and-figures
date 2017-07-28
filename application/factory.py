a87af9435358030372704d103ed5fc7546b095d1
Frankie Roberto <frankie@frankieroberto.com>
2017-07-28 15:18:40 +0100

    Remove extra blank line
    (fails code style check)

--- application/factory.py
+++ application/factory.py
@@ -124,7 +124,6 @@ def register_errorhandlers(app):
         # If a HTTPException, pull the `code` attribute; default to 500
         error_code = getattr(error, 'code', 500)
 
-
         if re.match("/cms", request.path):
             return render_template("error/{0}.html".format(error_code)), error_code
         else:
