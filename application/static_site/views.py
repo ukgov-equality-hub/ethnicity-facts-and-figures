6d97b80a4c4a15d74a335646617d2e00636d255f
Frankie Roberto <frankie@frankieroberto.com>
2017-08-16 10:53:56 +0100

    add newlines
    (code style)

--- application/static_site/views.py
+++ application/static_site/views.py
@@ -35,17 +35,20 @@ def index():
 def about_ethnicity():
     return render_template('static_site/about_ethnicity.html')
 
+
 @static_site_blueprint.route('/ethnicity_and_type_of_family_or_household')
 @internal_user_required
 @login_required
 def ethnicity_and_type_of_family_or_household():
     return render_template('static_site/ethnicity_and_type_of_family_or_household.html')
 
+
 @static_site_blueprint.route('/about-ethnicity/ethnic_groups_by_age')
 @internal_user_required
 @login_required
 def ethnic_groups_by_age():
     return render_template('static_site/about_ethnicity/ethnic_groups_by_age.html')
+
 
 @static_site_blueprint.route('/ethnic_groups_by_gender')
 @internal_user_required
