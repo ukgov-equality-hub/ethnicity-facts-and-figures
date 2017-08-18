7c22fe8a745b24a8f84e9e861f522db3576b799f
Frankie Roberto <frankie@frankieroberto.com>
2017-08-18 15:03:15 +0100

    refactor into private methods

--- application/static_site/filters.py
+++ application/static_site/filters.py
@@ -3,6 +3,7 @@ from hurry.filesize import size, alternative
 import markdown
 import jinja2
 
+
 def render_markdown(string):
     return Markup(markdown.markdown(string))
 
@@ -18,16 +19,28 @@ def filesize(string):
     except TypeError:
         return string
 
+
 def value_filter(value):
 
     icon_html = {
       "N/A": "<span class=\"not-applicable\">N/A<sup>*</sup></span>",
-      "?": "<span class=\"missing-data sample-too-small\"><span class=\"visually-hidden\">withheld because a small sample size makes it unreliable</span></span>",
-      "!": "<span class=\"missing-data confidential\"><span class=\"visually-hidden\">withheld to protect confidentiality</span></span>",
-      "-": "<span class=\"missing-data not-collected\"><span class=\"visually-hidden\">not collected</span></span>"
+      "?": __missing_data_icon("sample-too-small") +
+          __icon_explanation("withheld because a small sample size makes it unreliable"),
+      "!": __missing_data_icon("confidential") +
+          __icon_explanation("withheld to protect confidentiality"),
+      "-": __missing_data_icon("not-collected") +
+          __icon_explanation("not collected")
     }
 
     if value in icon_html:
         return icon_html[value]
     else:
         return jinja2.escape(value)
+
+
+def __missing_data_icon(class_name):
+    return "<span class=\"missing-data " + class_name + "\"></span>"
+
+
+def __icon_explanation(explanation):
+    return "<span class=\"visually-hidden\">" + explanation + "</span>"
