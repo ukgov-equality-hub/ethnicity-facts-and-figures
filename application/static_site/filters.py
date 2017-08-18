24308386b3861d1f963e9594f0b68cf23acd5eea
Frankie Roberto <frankie@frankieroberto.com>
2017-08-18 15:06:40 +0100

    code style

--- application/static_site/filters.py
+++ application/static_site/filters.py
@@ -25,11 +25,11 @@ def value_filter(value):
     icon_html = {
       "N/A": "<span class=\"not-applicable\">N/A<sup>*</sup></span>",
       "?": __missing_data_icon("sample-too-small") +
-          __icon_explanation("withheld because a small sample size makes it unreliable"),
+           __icon_explanation("withheld because a small sample size makes it unreliable"),
       "!": __missing_data_icon("confidential") +
-          __icon_explanation("withheld to protect confidentiality"),
+           __icon_explanation("withheld to protect confidentiality"),
       "-": __missing_data_icon("not-collected") +
-          __icon_explanation("not collected")
+           __icon_explanation("not collected")
     }
 
     if value in icon_html:
