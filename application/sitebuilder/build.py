1460e68c35898b494c55cf75ac8b9fba3d6c9c62
Andrew Kenyon <andrewkenyon123@gmail.com>
2017-08-29 16:48:37 +0100

    fix write_dimension_csv, used dimension.time_period instead of time_covered

--- application/sitebuilder/build.py
+++ application/sitebuilder/build.py
@@ -97,7 +97,7 @@ def write_versions(topic, topic_dir, subtopic, versions, application_url):
             output = write_dimension_csv(dimension=d,
                                          source=application_url,
                                          location=page.geographic_coverage,
-                                         time_period=d.time_covered,
+                                         time_period=d.time_period,
                                          data_source="%s %s" % (page.source_text, page.source_url))
             if d.title:
                 filename = '%s.csv' % d.title.lower().strip().replace(' ', '_').replace(',', '')
@@ -165,7 +165,7 @@ def build_measure_pages(page_service, subtopics, topic, topic_dir, beta_publicat
                 output = write_dimension_csv(dimension=d,
                                              source=application_url,
                                              location=measure_page.geographic_coverage,
-                                             time_period=d.time_covered,
+                                             time_period=d.time_period,
                                              data_source="%s %s" % (measure_page.source_text, measure_page.source_url))
                 if d.title:
                     filename = '%s.csv' % d.title.lower().strip().replace(' ', '_').replace(',', '')
