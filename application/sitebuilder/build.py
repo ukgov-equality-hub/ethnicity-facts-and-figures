25837b22eab871b7844d0cde1579152d1a899a32
Adam Shimali <adam.shimali@gmail.com>
2017-09-25 10:10:21 +0100

    Change gulpfile so that if you call gulp version on install (which is what happens in heroku) or on static build you will get the unversioned css and js output in static directories, and if versioning plugin fails pages can fallback to unversioned.

--- application/sitebuilder/build.py
+++ application/sitebuilder/build.py
@@ -335,12 +335,11 @@ def clear_up(build_dir):
 
 
 def create_versioned_assets(build_dir):
+    subprocess.run(['gulp', 'version'])
     static_dir = '%s/static' % build_dir
     if os.path.exists(static_dir):
         shutil.rmtree(static_dir)
     shutil.copytree(current_app.static_folder, static_dir)
-    subprocess.run(['gulp', 'sass'])
-    subprocess.run(['gulp', 'scripts'])
 
 
 def _filter_out_subtopics_with_no_ready_measures(subtopics, beta_publication_states):
--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,33 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(rev())
-    .pipe(gulp.dest('./application/static/stylesheets'))
-    .pipe(rev.manifest())
     .pipe(gulp.dest('./application/static/stylesheets'))
 });
 
 gulp.task('scripts', function() {
   return gulp.src(['./application/static/vendor/polyfills/*.js', './application/src/js/*.js'], [])
     .pipe(concat('all.js'))
+    .pipe(gulp.dest('./application/static/javascripts'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./application/src/js/*.js', './application/src/sass/*.scss','./application/src/sass/*.css', './application/src/sass/**/*.scss', './application/src/sass/**/**/*.scss'], ['version']);
+});
+
+gulp.task('version-css', ['sass'], function () {
+  return gulp.src(['./application/static/stylesheets/application.css', './application/static/stylesheets/cms.css'])
+    .pipe(rev())
+    .pipe(gulp.dest('./application/static/stylesheets'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('./application/static/stylesheets'))
+});
+
+gulp.task('version-js', ['scripts'],  function () {
+  return gulp.src([ './application/static/javascripts/all.js'])
     .pipe(rev())
     .pipe(gulp.dest('./application/static/javascripts'))
     .pipe(rev.manifest())
     .pipe(gulp.dest('./application/static/javascripts'))
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./application/src/js/*.js', './application/src/sass/*.scss','./application/src/sass/*.css', './application/src/sass/**/*.scss', './application/src/sass/**/**/*.scss'], ['sass', 'scripts']);
-});
+gulp.task('version', ['sass', 'scripts', 'version-css', 'version-js'])
