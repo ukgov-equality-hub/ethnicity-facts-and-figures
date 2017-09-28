07fe6c4289c73eb901433a7e31ea1998408f8a91
Adam Shimali <adam.shimali@gmail.com>
2017-09-28 14:14:47 +0100

    Enable sqreen for on cms for heroku apps

--- application/wsgi.py
+++ application/wsgi.py
@@ -1,3 +1,9 @@
+import os
+
+if os.environ.get('SQREEN_TOKEN') is not None:
+    import sqreen
+    sqreen.start()
+
 from application.config import Config
 from application.factory import create_app
 
