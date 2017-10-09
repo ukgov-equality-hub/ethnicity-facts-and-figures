8a716de4ddcf1ffe850ea7176dbb4840564c2c0f
Frankie Roberto <frankie@frankieroberto.com>
2017-10-09 15:39:09 +0100

    Remove redundant javascript

--- application/static/javascripts/all.js
+++ application/static/javascripts/all.js
@@ -1166,11 +1166,6 @@ function TableWithFixedHeader(tableElement) {
 
 
 $(document).ready(function () {
-  var $details = $('details');
-  $details.each(function () {
-    $(this).after('<div class="print-node"><h3 class="heading-small">' + $(this).find('span').html() + '</h3><div>' + $(this).find('.panel').html() + '</div></div>')
-  });
-
   $('.js--print').click(function (e) {
     e.preventDefault();
     window.print();
