4328053dbe377af2b3e4d7c9fdfcaca178ef5371
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-10-18 14:18:10 +0100

    add /latest route to api view

--- application/static_site/views.py
+++ application/static_site/views.py
@@ -138,7 +138,10 @@ def measure_page_json(topic, subtopic, measure, version):
     subtopic_guid = 'subtopic_%s' % subtopic.replace('-', '')
 
     try:
-        page = page_service.get_page_by_uri_and_version(subtopic_guid, measure, version)
+        if version == 'latest':
+            page = page_service.get_latest_version(subtopic_guid, measure)
+        else:
+            page = page_service.get_page_by_uri_and_version(subtopic_guid, measure, version)
     except PageNotFoundException:
         abort(404)
     if current_user.is_departmental_user():
