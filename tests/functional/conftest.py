b222f0fc2a4b9b864fb4cef396025bc58766de55
Adam Shimali <adam.shimali@gmail.com>
2017-11-23 16:36:01 +0000

    change to correct env var name

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -31,11 +31,11 @@ def _driver():
         # This is for CI, heroku chrome buildpack sets GOOGLE_CHROME_BIN itself
         # but we need to set CHROMEDRIVER_PATH ourselves so make sure env variable
         # for that is set correctly
-        GOOGLE_CHROME_BIN = os.environ['GOOGLE_CHROME_BIN']
+        GOOGLE_CHROME_SHIM = os.environ['GOOGLE_CHROME_SHIM']
         CHROMEDRIVER_PATH = os.environ['CHROMEDRIVER_PATH']
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
-        options.binary_location = GOOGLE_CHROME_BIN
+        options.binary_location = GOOGLE_CHROME_SHIM
         driver = webdriver.Chrome(chrome_options=options,
                                   executable_path=CHROMEDRIVER_PATH)
 
