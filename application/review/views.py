531661b9f649a4a4b4ea16cdad1a0ed9c791a91e
Adam Shimali <adam.shimali@gmail.com>
2018-02-12 16:36:45 +0000

    Handle errors in generate review url view method.
    Protect generate review url.

--- application/review/views.py
+++ application/review/views.py
@@ -1,16 +1,16 @@
 from flask import render_template, current_app, abort, flash, url_for
+from flask_login import login_required
 from itsdangerous import SignatureExpired, BadTimeSignature
 from sqlalchemy.orm.exc import NoResultFound
 
 from application.cms.models import Page
 from application.review import review_blueprint
-from application.utils import decode_review_token, generate_review_token
+from application.utils import decode_review_token, generate_review_token, internal_user_required
 from application import db
 
 
 @review_blueprint.route('/<review_token>')
 def review_page(review_token):
-
     try:
         id, version = decode_review_token(review_token, current_app.config)
         page = Page.query.filter_by(guid=id, version=version, review_token=review_token).one()
@@ -36,13 +36,18 @@ def review_page(review_token):
 
 
 @review_blueprint.route('/new-review-url/<id>/<version>')
+@internal_user_required
+@login_required
 def get_new_review_url(id, version):
-    token = generate_review_token(id, version)
-    page = Page.query.filter_by(guid=id, version=version).one()
-    page.review_token = token
-    db.session.add(page)
-    db.session.commit()
-    url = url_for('review.review_page', review_token=page.review_token, _external=True)
-    expires = page.review_token_expires_in(current_app.config)
-    return "<p>Preview for deparmental reviewers - expires in {expires} days<br>{url}</p>".format(expires=expires,
-                                                                                                  url=url)
+    try:
+        token = generate_review_token(id, version)
+        page = Page.query.filter_by(guid=id, version=version).one()
+        page.review_token = token
+        db.session.add(page)
+        db.session.commit()
+        url = url_for('review.review_page', review_token=page.review_token, _external=True)
+        expires = page.review_token_expires_in(current_app.config)
+        return "<p>Preview for reviewers - expires in {expires} days<br>{url}</p>".format(expires=expires, url=url)
+    except Exception as e:
+        current_app.logger.exception(e)
+        abort(500)
