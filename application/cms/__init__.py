0f881ce3a79e96293913514b007379e98ef47f7e
Adam Shimali <adam.shimali@gmail.com>
2018-02-21 14:09:01 +0000

    Added asset path for cms templates

--- application/cms/__init__.py
+++ application/cms/__init__.py
@@ -3,3 +3,8 @@ from flask import Blueprint
 cms_blueprint = Blueprint('cms', __name__, url_prefix='/cms')
 
 from application.cms.views import create_measure_page  # noqa
+
+
+@cms_blueprint.context_processor
+def asset_path_context_processor():
+    return {'asset_path': '/static/'}
