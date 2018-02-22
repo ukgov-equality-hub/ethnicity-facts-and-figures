2c98358e44a320c20ca2346eda6cf6d6df7eef52
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2018-02-22 13:37:19 +0000

    pep8

--- application/cms/views.py
+++ application/cms/views.py
@@ -56,6 +56,7 @@ from application.cms.categorisation_service import categorisation_service
 from application.utils import get_bool, internal_user_required, admin_required
 from application.sitebuilder import build_service
 
+
 @cms_blueprint.route('/', methods=['GET', 'POST'])
 @internal_user_required
 @login_required
