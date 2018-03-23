a5dfa4752836ec27b25467410cdfac2c0fb0b7f8
Adam Shimali <adam.shimali@gmail.com>
2018-03-23 13:52:11 +0000

    Ensure up and down migrations work. Merge two refactor migrations.

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,4 +1,4 @@
-from flask_security import UserMixin
+from flask_security import UserMixin, RoleMixin
 from sqlalchemy.dialects.postgresql import ARRAY
 from application import db
 from sqlalchemy.ext.mutable import MutableList
