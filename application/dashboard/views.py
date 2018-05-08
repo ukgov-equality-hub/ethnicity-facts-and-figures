8f2a121904e2e107357d7b7428a3ffed99d9971d
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2018-05-08 11:33:16 +0100

    update to pluralise dashboard

--- application/dashboard/views.py
+++ application/dashboard/views.py
@@ -95,11 +95,11 @@ def measure_progress():
         review_count = len([measure for measure in measure_cards if measure['stage'] == 'review'])
         published_count = len([measure for measure in measure_cards if measure['stage'] == 'published'])
 
-        return render_template('dashboard/measure_progress.html', measures=measure_cards, planned_count=planned_count,
+        return render_template('dashboards/measure_progress.html', measures=measure_cards, planned_count=planned_count,
                                progress_count=progress_count, review_count=review_count,
                                published_count=published_count)
     else:
-        return render_template('dashboard/measure_progress.html', measures=[], planned_count=0,
+        return render_template('dashboards/measure_progress.html', measures=[], planned_count=0,
                                progress_count=0, review_count=0, published_count=0)
 
 
