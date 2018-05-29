d58e27fa0a984c9143139effcb7d62ded52ab299
Frankie Roberto <frankie@frankieroberto.com>
2018-05-29 10:54:56 +0100

    Add privacy policy

--- application/static_site/views.py
+++ application/static_site/views.py
@@ -66,6 +66,13 @@ def cookies():
     return render_template('static_site/static_pages/cookies.html')
 
 
+@static_site_blueprint.route('/privacy-policy')
+@internal_user_required
+@login_required
+def privacy_policy():
+    return render_template('static_site/static_pages/privacy-policy.html')
+
+
 @static_site_blueprint.route('/<uri>')
 @internal_user_required
 @login_required
