90fdc37e88ad3341044c3332cf277230d7dcf57f
Kev Keenoy <kevin.keenoy@cabinetoffice.gov.uk>
2018-06-27 15:42:19 +0100

    Don't pass `asset_path` into templates, as they all now use `/static/`

--- application/static_site/__init__.py
+++ application/static_site/__init__.py
@@ -3,8 +3,3 @@ from flask import Blueprint
 static_site_blueprint = Blueprint('static_site', __name__)
 
 from application.static_site.views import index  # noqa
-
-
-@static_site_blueprint.context_processor
-def asset_path_context_processor():
-    return {'asset_path': '/static/'}
