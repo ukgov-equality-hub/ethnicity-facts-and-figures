0c5522800be526935fc6201033811a2da6259f34
Samuel Williams <samuelhwilliams@gmail.com>
2018-07-20 13:24:39 +0100

    Add `data_migration` management command
     ## Summary
    This adds a `data_migration` command to the manager script, and removes
    the alembic migration that adds the population topic to a static sql
    file that can be referenced from the aforementioned command.

---
+++ scripts/data_migrations/population-topic.sql
@@ -0,0 +1,8 @@
+INSERT INTO page (guid, title, uri, page_type, description, additional_description, version, created_at, parent_guid, parent_version, db_version_id)
+    VALUES ('topic_population', 'British population', 'british-population', 'topic', 'Population statistics and Census data, also analysed by age, location and other factors', 'The government collects population data through the Census, which happens every 10 years, and other surveys.
+
+Find population statistics and demographic information for different ethnic groups in England and Wales. We have included data for Scotland where it’s available.', '1.0', now(), 'homepage', '1.0', 1);
+
+INSERT INTO page (guid, title, uri, page_type, description, version, created_at, db_version_id, parent_guid, parent_version, position)
+    VALUES ('subtopic_national_and_regional_populations', 'National and regional populations', 'national-and-regional-populations', 'subtopic', 'National and regional populations', '1.0', now(), 1, 'topic_population', '1.0', 0),
+           ('subtopic_demographics', 'Demographics', 'demographics', 'subtopic', 'Demographics', '1.0', now(), 1, 'topic_population', '1.0', 0);
