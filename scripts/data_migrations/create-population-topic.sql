03574029f91606f075d3cd300861061bc5283e43
Samuel Williams <samuelhwilliams@gmail.com>
2018-07-26 11:57:51 +0100

    Order subtopics correctly
     ## Summary
    National and regional populations should be ordered before Demographics.

--- scripts/data_migrations/create-population-topic.sql
+++ scripts/data_migrations/create-population-topic.sql
@@ -4,5 +4,5 @@ INSERT INTO page (guid, title, uri, page_type, description, additional_descripti
 Find population statistics and demographic information for different ethnic groups in England and Wales. We have included data for Scotland where it’s available.', '1.0', now(), 'homepage', '1.0', 1);
 
 INSERT INTO page (guid, title, uri, page_type, description, version, created_at, db_version_id, parent_guid, parent_version, position)
-    VALUES ('subtopic_national_and_regional_populations', 'National and regional populations', 'national-and-regional-populations', 'subtopic', 'National and regional populations', '1.0', now(), 1, 'topic_population', '1.0', 0),
-           ('subtopic_demographics', 'Demographics', 'demographics', 'subtopic', 'Demographics', '1.0', now(), 1, 'topic_population', '1.0', 0);
+    VALUES ('subtopic_national_and_regional_populations', 'National and regional populations', 'national-and-regional-populations', 'subtopic', 'National and regional populations', '1.0', now(), 1, 'topic_population', '1.0', 1),
+           ('subtopic_demographics', 'Demographics', 'demographics', 'subtopic', 'Demographics', '1.0', now(), 1, 'topic_population', '1.0', 2);
