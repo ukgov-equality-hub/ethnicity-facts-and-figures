05a1d975c8441ec50a0eab154531f33e7dd07095
Frankie Roberto <frankie@frankieroberto.com>
2018-07-26 14:52:24 +0100

    codestyle fix

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -11,7 +11,9 @@ def test_homepage_includes_mailing_list_sign_up(test_app_client):
     assert response.status_code == 200
     page = BeautifulSoup(response.get_data(as_text=True), 'html.parser')
 
-    assert page.select_one("form[action=" + config.NEWSLETTER_SUBSCRIBE_URL "]"), "Mailing list subscription form should be present"
+    assert page.select_one("form[action=" + config.NEWSLETTER_SUBSCRIBE_URL "]"), \
+        "Mailing list subscription form should be present"
+
     assert page.select_one('label', text='Email address'), "E-mail address label should be present"
     assert page.select_one('input[name=EMAIL]'), "E-mail address field should be present"
     assert page.select_one('button', text='Subscribe'), "Subscribe button should be present"
