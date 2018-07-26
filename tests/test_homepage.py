7afd98366266f65a2eb87c01085eeadeab9ce725
Frankie Roberto <frankie@frankieroberto.com>
2018-07-26 14:22:06 +0100

    Code style

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -12,8 +12,7 @@ def test_homepage_includes_mailing_list_sign_up(test_app_client):
     page = BeautifulSoup(response.get_data(as_text=True), 'html.parser')
 
 
-    assert page.select("form[action=" + config.NEWSLETTER_SUBSCRIBE_URL "]"),
-        "Mailing list subscription form should be present"
+    assert page.select("form[action=" + config.NEWSLETTER_SUBSCRIBE_URL "]"), "Mailing list subscription form should be present"
     assert page.select('label', text='Email address'), "E-mail address label should be present"
     assert page.select('input[name=EMAIL]'), "E-mail address field should be present"
     assert page.select('button', text='Subscribe'), "Subscribe button should be present"
