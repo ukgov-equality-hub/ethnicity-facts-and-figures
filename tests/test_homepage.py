0f6c8cec1b4c87a37a3e2d9d87c3a283ac44b636
Frankie Roberto <frankie@frankieroberto.com>
2018-07-26 14:37:14 +0100

    Select one only

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -11,7 +11,7 @@ def test_homepage_includes_mailing_list_sign_up(test_app_client):
     assert response.status_code == 200
     page = BeautifulSoup(response.get_data(as_text=True), 'html.parser')
 
-    assert page.select("form[action=" + config.NEWSLETTER_SUBSCRIBE_URL "]"), "Mailing list subscription form should be present"
-    assert page.select('label', text='Email address'), "E-mail address label should be present"
-    assert page.select('input[name=EMAIL]'), "E-mail address field should be present"
-    assert page.select('button', text='Subscribe'), "Subscribe button should be present"
+    assert page.select_one("form[action=" + config.NEWSLETTER_SUBSCRIBE_URL "]"), "Mailing list subscription form should be present"
+    assert page.select_one('label', text='Email address'), "E-mail address label should be present"
+    assert page.select_one('input[name=EMAIL]'), "E-mail address field should be present"
+    assert page.select_one('button', text='Subscribe'), "Subscribe button should be present"
