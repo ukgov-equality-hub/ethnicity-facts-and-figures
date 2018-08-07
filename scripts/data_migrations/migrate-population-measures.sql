f6a8f19434a3abe0bdd1b50b4c12bba403f7ea0b
Samuel Williams <samuelhwilliams@gmail.com>
2018-08-07 14:33:11 +0100

    Add migration for existing population topics
     ## Summary
    Moves the existing population topics, 'English language skills' and
    'People living in deprived neighbourhoods', to the British Population ->
    Demographics subtopic.
    
     ## Summary
    https://trello.com/c/CrOTTaRE/861

---
+++ scripts/data_migrations/migrate-population-measures.sql
@@ -0,0 +1,2 @@
+UPDATE page SET parent_guid = 'subtopic_demographics' WHERE parent_guid = 'subtopic_community' and uri = 'english-language-skills';
+UPDATE page SET parent_guid = 'subtopic_demographics' WHERE parent_guid = 'subtopic_community' and uri = 'people-living-in-deprived-neighbourhoods';
