6b6d1d7575a8485ca4caf0d9d6e5de74bdc36f9f
Samuel Williams <samuelhwilliams@gmail.com>
2018-08-08 12:43:35 +0100

    Call manage.py script to create redirects; platform agnostic

--- scripts/data_migrations/business-self-employment-redirects.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for app in rd-cms-dev rd-cms-staging rd-cms; do
-  heroku run -a ${app} ./manage.py add_redirect_rule --from_uri 'work-pay-and-benefits/business-and-self-employment/self-employment' --to_uri 'workforce-and-business/business-and-self-employment/self-employment'
-  heroku run -a ${app} ./manage.py add_redirect_rule --from_uri 'work-pay-and-benefits/business-and-self-employment/access-to-start-up-loans' --to_uri 'workforce-and-business/business-and-self-employment/access-to-start-up-loans'
-done
