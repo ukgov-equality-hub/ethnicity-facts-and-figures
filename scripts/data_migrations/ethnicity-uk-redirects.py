75d67cf31578c479b72c61d37c05c78456e95943
Samuel Williams <samuelhwilliams@gmail.com>
2018-08-08 15:32:23 +0100

    fix codestyle

--- scripts/data_migrations/ethnicity-uk-redirects.py
+++ scripts/data_migrations/ethnicity-uk-redirects.py
@@ -3,9 +3,9 @@
 import subprocess
 
 redirect_pairs = [
-    ('ethnicity-in-the-uk/population-by-ethnicity', 'british-population/national-and-regional-populations/population-of-england-and-wales/latest'),
-    ('ethnicity-in-the-uk/ethnic-groups-by-region', 'british-population/national-and-regional-populations/regional-ethnic-diversity/latest'),
-    ('ethnicity-in-the-uk/ethnic-groups-by-gender', 'british-population/demographics/male-and-female-populations/latest'),
+    ('ethnicity-in-the-uk/population-by-ethnicity', 'british-population/national-and-regional-populations/population-of-england-and-wales/latest'),  # noqa
+    ('ethnicity-in-the-uk/ethnic-groups-by-region', 'british-population/national-and-regional-populations/regional-ethnic-diversity/latest'),  # noqa
+    ('ethnicity-in-the-uk/ethnic-groups-by-gender', 'british-population/demographics/male-and-female-populations/latest'),  # noqa
 ]
 
 for from_uri, to_uri in redirect_pairs:
