efca0a942d248d26e687583418319d2c263e9a4c
Samuel Williams <samuelhwilliams@gmail.com>
2018-09-20 11:16:59 +0100

    Fix positioning of search box in header
     ## Summary
    The site search box in the header was slightly inset from the right
    edge, making alignment of other elements (e.g. the 'Sign out' link
    above) look slightly jarring.
    
    Now that said 'Sign out' link is no longer in the main header block, we
    can move the search box out into a separate one-third header block,
    making alignment easier. This includes a fix for the media queries that
    provides a better experience for some of our middle media sizes (e.g.
    tablet) - particularly in the 641px to 769px range.
    
     ## Ticket
    https://trello.com/c/yHCN97l3/1035

--- tests/test_search.py
+++ tests/test_search.py
@@ -12,7 +12,7 @@ def test_homepage_search_links_to_google_custom_url_before_javascript(
     assert resp.status_code == 200
     page = BeautifulSoup(resp.get_data(as_text=True), "html.parser")
 
-    search_forms = page.header.select("form#search-form")
+    search_forms = page.header.select("#search-form")
     assert len(search_forms) == 1
 
     assert search_forms[0]["action"] == "https://cse.google.com/cse/publicurl"
