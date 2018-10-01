f5d707b4fc1c85fa0afbf71118708ced979c3f92
Frankie Roberto <frankie@frankieroberto.com>
2018-10-01 15:20:29 +0100

    Remove 'finder' prefix from variable names

--- application/data/ethnicity_classification_synchroniser.py
+++ application/data/ethnicity_classification_synchroniser.py
@@ -13,11 +13,11 @@ class EthnicityClassificationSynchroniser:
     def __init__(self, classification_service):
         self.classification_service = classification_service
 
-    def synchronise_classifications(self, finder_classification_collection):
-        classifications = finder_classification_collection.get_classifications()
+    def synchronise_classifications(self, ethnicity_classification_collection):
+        classifications = ethnicity_classification_collection.get_classifications()
         classifications.sort(key=lambda classification: self.__split_classification_code(classification.code))
 
-        for index, classification in enumerate(finder_classification_collection.get_classifications()):
+        for index, classification in enumerate(ethnicity_classification_collection.get_classifications()):
             if classification.code.endswith("+") is not True:
                 self.__synchronise_classification(classification, position=index)
 
@@ -27,39 +27,39 @@ class EthnicityClassificationSynchroniser:
         digits = [character for character in classification_code if character.isdigit()]
         if len(digits) > 0:
             number_part = int("".join(digits))
-            return number_part, classification_code[len(digits) :]
+            return number_part, classification_code[len(digits) : ]
         else:
             return 1000, classification_code
 
-    def __synchronise_classification(self, finder_classification, position=999):
+    def __synchronise_classification(self, classification, position=999):
         try:
-            self.__update_database_classification(finder_classification, position=position)
+            self.__update_database_classification(classification, position=position)
         except ClassificationNotFoundException:
-            self.__create_database_classification(finder_classification, position=position)
+            self.__create_database_classification(classification, position=position)
 
-    def __update_database_classification(self, finder_classification, position):
+    def __update_database_classification(self, classification, position):
         database_classification = self.classification_service.get_classification_by_code(
-            "Ethnicity", finder_classification.code
+            "Ethnicity", classification.code
         )
-        database_classification.title = finder_classification.name
-        database_classification.long_title = finder_classification.long_name
+        database_classification.title = classification.name
+        database_classification.long_title = classification.long_name
         database_classification.subfamily = ""
         database_classification.position = position
         self.classification_service.update_classification(database_classification)
 
-    def __create_database_classification(self, finder_classification, position):
-        values = finder_classification.get_display_values()
-        if finder_classification.has_parent_child_relationship():
-            parents = finder_classification.get_parent_values()
+    def __create_database_classification(self, classification, position):
+        values = classification.get_display_values()
+        if classification.has_parent_child_relationship():
+            parents = classification.get_parent_values()
         else:
             parents = []
 
         self.classification_service.create_classification_with_values(
-            finder_classification.code,
+            classification.code,
             "Ethnicity",
             "",
-            finder_classification.name,
-            long_title=finder_classification.long_name,
+            classification.name,
+            long_title=classification.long_name,
             position=position,
             values=values,
             values_as_parent=parents,
