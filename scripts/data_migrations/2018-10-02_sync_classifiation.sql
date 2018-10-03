e288bce6154f3b619404930b7f731b59e9a64b74
Frankie Roberto <frankie@frankieroberto.com>
2018-10-03 16:14:00 +0100

    Update the data migration

--- scripts/data_migrations/2018-10-02_sync_classifiation.sql
+++ scripts/data_migrations/2018-10-02_sync_classifiation.sql
@@ -1,40 +1,40 @@
 -- Delete the classification currently used only for "Students aged 16 to 18 achieving 3 A grades or better at A level"
 -- This measure should actually be tagged with the standard DfE 18 category
 DELETE from dimension_categorisation
-USING categorisation
-WHERE categorisation.id = dimension_categorisation.categorisation_id AND categorisation.code = '19A';
-DELETE from association
-USING categorisation
-WHERE categorisation.id = association.categorisation_id AND categorisation.code = '19A';
-DELETE from parent_association
-USING categorisation
-WHERE categorisation.id = parent_association.categorisation_id AND categorisation.code = '19A';
-DELETE from categorisation
-WHERE categorisation.code = '19A';
+USING classification
+WHERE classification.id = dimension_categorisation.classification_id AND classification.code = '19A';
+DELETE from ethnicity_in_classification
+USING classification
+WHERE classification.id = ethnicity_in_classification.classification_id AND classification.code = '19A';
+DELETE from parent_ethnicity_in_classification
+USING classification
+WHERE classification.id = parent_ethnicity_in_classification.classification_id AND classification.code = '19A';
+DELETE from classification
+WHERE classification.code = '19A';
 
 -- Delete unused classification "5 - Black, Indian, Pakistani and Bangladeshi, White, Other inc Mixed"
 DELETE from dimension_categorisation
-USING categorisation
-WHERE categorisation.id = dimension_categorisation.categorisation_id AND categorisation.code = '5D';
-DELETE from association
-USING categorisation
-WHERE categorisation.id = association.categorisation_id AND categorisation.code = '5D';
-DELETE from parent_association
-USING categorisation
-WHERE categorisation.id = parent_association.categorisation_id AND categorisation.code = '5D';
-DELETE from categorisation
-WHERE categorisation.code = '5D';
+USING classification
+WHERE classification.id = dimension_categorisation.classification_id AND classification.code = '5D';
+DELETE from ethnicity_in_classification
+USING classification
+WHERE classification.id = ethnicity_in_classification.classification_id AND classification.code = '5D';
+DELETE from parent_ethnicity_in_classification
+USING classification
+WHERE classification.id = parent_ethnicity_in_classification.classification_id AND classification.code = '5D';
+DELETE from classification
+WHERE classification.code = '5D';
 
 -- Delete unused classification "Bangladeshi, Chinese, Indian, Pakistani, Asian other, Black, Mixed, White British, White other, Other, Arab, Gypsy/Traveller/Irish Traveller"
 DELETE from dimension_categorisation
-USING categorisation
-WHERE categorisation.id = dimension_categorisation.categorisation_id AND categorisation.code = '11A';
-DELETE from association
-USING categorisation
-WHERE categorisation.id = association.categorisation_id AND categorisation.code = '11A';
-DELETE from parent_association
-USING categorisation
-WHERE categorisation.id = parent_association.categorisation_id AND categorisation.code = '11A';
-DELETE from categorisation
-WHERE categorisation.code = '11A';
+USING classification
+WHERE classification.id = dimension_categorisation.classification_id AND classification.code = '11A';
+DELETE from ethnicity_in_classification
+USING classification
+WHERE classification.id = ethnicity_in_classification.classification_id AND classification.code = '11A';
+DELETE from parent_ethnicity_in_classification
+USING classification
+WHERE classification.id = parent_ethnicity_in_classification.classification_id AND classification.code = '11A';
+DELETE from classification
+WHERE classification.code = '11A';
 
