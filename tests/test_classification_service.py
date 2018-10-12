a90de3705635c972a59bf036e1e1477e9f683f69
Kev Keenoy <kevin.keenoy@cabinetoffice.gov.uk>
2018-10-12 14:38:34 +0100

    Update clasification_service tests to no be about geography
    These tests have become confusing now that all our classifications are
    about ethnicity, so have re-worked the same tests with some test data
    more closely resembling ethnicity than London boroughs do.

--- tests/test_classification_service.py
+++ tests/test_classification_service.py
@@ -7,37 +7,29 @@ from application.cms.models import Classification, Ethnicity
 classification_service = ClassificationService()
 
 
-def build_london_boroughs():
-    classification_service.create_classification_with_values(
-        "L1", "Local level", "Greater London Boroughs", values=["Barnet", "Camden", "Ealing", "Haringey"]
-    )
-    classification_service.create_classification_with_values(
-        "L2", "Local level", "Inner London Boroughs", values=["Camden", "Haringey"]
-    )
-
-
-def build_colours():
-    classification_service.create_classification_with_values("C1", "Colours", "Paint", "Cars", values=["Red", "Black"])
-    classification_service.create_classification_with_values("C2", "Colours", "Paint", "Nails", values=["Pink", "Red"])
+def build_a_number_of_ethnicity_clasifications(number):
+    for num in range(number):
+        # Add one to make 1-based classifications
+        classification_service.create_classification(f"C{num+1}", "Subfamily", f"Classification {num+1}")
 
 
 def test_get_classification_by_id_does_return_classification(db_session):
     # given the london boroughs classifications
-    build_london_boroughs()
+    build_a_number_of_ethnicity_clasifications(2)
 
     # when we get classifications by code using the classification service
-    expect_greater_london = classification_service.get_classification_by_id("L1")
-    expect_inner_london = classification_service.get_classification_by_id("L2")
+    expect_c1 = classification_service.get_classification_by_id("C1")
+    expect_c2 = classification_service.get_classification_by_id("C2")
 
     # then we expect to get the correct classifications returned
-    assert expect_greater_london.title == "Greater London Boroughs"
-    assert expect_inner_london.title == "Inner London Boroughs"
+    assert expect_c1.title == "Classification 1"
+    assert expect_c2.title == "Classification 2"
 
 
 def test_create_classification(db_session):
     assert not Classification.query.all()
 
-    classification = classification_service.create_classification("GeoA", "", "Region")
+    classification = classification_service.create_classification("2B", "", "White and Other")
 
     assert classification == Classification.query.all()[0]
 
@@ -45,85 +37,87 @@ def test_create_classification(db_session):
 def test_delete_classification_removes_classification(db_session):
     # Given some classifications
     assert not Classification.query.all()
-    classification_service.create_classification("G1", "Regional Geography", "Region 1")
-    classification_service.create_classification("G2", "Regional Geography", "Region 2")
-    classification_service.create_classification("G3", "Regional Geography", "Region 3")
-    classification_service.create_classification("G4", "Regional Geography", "Region 4")
+    build_a_number_of_ethnicity_clasifications(4)
+    assert Classification.query.count() == 4
 
     # When we delete a classification
-    classification = classification_service.get_classification_by_id("G3")
+    classification = classification_service.get_classification_by_id("C3")
     assert classification is not None
     classification_service.delete_classification(classification=classification)
 
     # Then it should be deleted
     with pytest.raises(ClassificationNotFoundException):
-        classification_service.get_classification_by_id("G3")
+        classification_service.get_classification_by_id("C3")
     assert Classification.query.count() == 3
 
 
-def test_create_value_creates_a_value(db_session):
+def test_create_or_get_value_creates_a_value(db_session):
     assert not Ethnicity.query.all()
 
-    value = classification_service.create_or_get_value("Camden")
+    value = classification_service.create_or_get_value("Purple")
 
     assert value is not None
-    assert value.value == "Camden"
+    assert value.value == "Purple"
 
 
 def test_create_or_get_value_recalls_existing_value(db_session):
-    # given a setup with one
+    # given a setup with one Ethnicity value
     assert not Ethnicity.query.all()
-    value = classification_service.create_or_get_value("Camden")
+    value = classification_service.create_or_get_value("Green")
 
-    # when we recall the value
-    value_recalled = classification_service.create_or_get_value("Camden")
+    # when we recall the same value
+    value_recalled = classification_service.create_or_get_value("Green")
 
-    # then the
+    # then the original Ethnicity row is recalled
     assert value.id == value_recalled.id
     assert Ethnicity.query.count() == 1
 
 
-def test_add_value_to_classification_appends_new_value(db_session):
-    # given a setup with one
-    classification_service.create_classification("G1", "Local level", "Greater London Boroughs")
-    classification_service.create_classification("G2", "Local level", "Inner London Boroughs")
+def test_add_values_to_classification_appends_new_values(db_session):
+    # given a setup with two empty classifications
+    build_a_number_of_ethnicity_clasifications(2)
 
-    greater_london = classification_service.get_classification_by_id("G1")
-    inner_london = classification_service.get_classification_by_id("G2")
+    classification_1 = classification_service.get_classification_by_id("C1")
+    classification_2 = classification_service.get_classification_by_id("C2")
+    assert len(classification_1.ethnicities) == 0
+    assert len(classification_2.ethnicities) == 0
 
-    classification_service.add_values_to_classification(greater_london, ["Barnet", "Camden", "Haringey"])
-    classification_service.add_values_to_classification(inner_london, ["Camden", "Haringey"])
+    # when we add some values to the empty classifications
+    classification_service.add_values_to_classification(classification_1, ["Green", "Red", "Purple"])
+    classification_service.add_values_to_classification(classification_2, ["Red", "Pink"])
 
-    # then the
-    greater_london = classification_service.get_classification_by_id("G1")
-    inner_london = classification_service.get_classification_by_id("G2")
-    camden = classification_service.create_or_get_value("Camden")
+    # then the values have been added
+    assert len(classification_1.ethnicities) == 3
+    assert len(classification_2.ethnicities) == 2
 
-    assert len(camden.classifications) == 2
-    assert len(greater_london.ethnicities) == 3
-    assert len(inner_london.ethnicities) == 2
+    # And all classifications containing a particular value can be found
+    red = classification_service.create_or_get_value("Red")
+    pink = classification_service.create_or_get_value("Pink")
+    assert len(red.classifications) == 2
+    assert len(pink.classifications) == 1
+    assert set(red.classifications) == set([classification_1, classification_2])
+    assert pink.classifications == [classification_2]
 
 
 def test_remove_value_from_classification_removes_value(db_session):
-    # given a setup with one
-    greater_london = classification_service.create_classification("G1", "Local level", "Greater London Boroughs")
-    inner_london = classification_service.create_classification("G2", "Local level", "Inner London Boroughs")
+    # given a setup with two classifications with values
+    build_a_number_of_ethnicity_clasifications(2)
+    classification_1 = classification_service.get_classification_by_id("C1")
+    classification_2 = classification_service.get_classification_by_id("C2")
+    classification_service.add_values_to_classification(classification_1, ["Green", "Red", "Purple"])
+    classification_service.add_values_to_classification(classification_2, ["Red", "Pink"])
 
-    classification_service.add_values_to_classification(greater_london, ["Barnet", "Camden", "Haringey"])
-    classification_service.add_values_to_classification(inner_london, ["Camden", "Haringey"])
+    # when we remove a value from one classification that also exisits in the second classification
+    classification_service.remove_value_from_classification(classification_2, "Red")
 
-    # when we remove the value
-    classification_service.remove_value_from_classification(inner_london, "Camden")
-
-    # then the
-    camden = classification_service.get_value("Camden")
-
-    assert len(camden.classifications) == 1
-    assert len(greater_london.ethnicities) == 3
-    assert len(inner_london.ethnicities) == 1
-    assert "Inner London Boroughs" not in [c.title for c in camden.classifications]
-    assert "Camden" not in [c.value for c in inner_london.ethnicities]
-    assert "Camden" in [c.value for c in greater_london.ethnicities]
+    # then the value is gone from the specified classification but still exists in the other classification
+    red = classification_service.get_value("Red")
+    assert len(red.classifications) == 1
+    assert len(classification_1.ethnicities) == 3
+    assert len(classification_2.ethnicities) == 1
+    assert "Classification 2" not in [c.title for c in red.classifications]
+    assert "Red" not in [c.value for c in classification_2.ethnicities]
+    assert "Red" in [c.value for c in classification_1.ethnicities]
 
 
 def test_add_parent_value_to_classification_appends_new_parent(db_session):
