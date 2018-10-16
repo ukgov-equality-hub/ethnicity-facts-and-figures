f9585ef1eb3ab2fe7eb532501db96f9407af28f1
Frankie Roberto <frankie@frankieroberto.com>
2018-10-16 18:04:32 +0100

    Refactor into more macros

--- tests/test_jinja_filters.py
+++ tests/test_jinja_filters.py
@@ -20,20 +20,19 @@ class TestRenderMarkdown:
 
 
 class TestIndexOfLastInitialZero:
-
     def test_when_only_one_zero(self):
-        assert index_of_last_initial_zero([0,10,20]) == 0
+        assert index_of_last_initial_zero([0, 10, 20]) == 0
 
     def test_when_many_zeros(self):
-        assert index_of_last_initial_zero([0,0,0,0,1,2]) == 3
+        assert index_of_last_initial_zero([0, 0, 0, 0, 1, 2]) == 3
 
     def test_when_later_zeros_are_present(self):
-        assert index_of_last_initial_zero([0,0,1,2,1,0]) == 1
+        assert index_of_last_initial_zero([0, 0, 1, 2, 1, 0]) == 1
 
     def test_when_no_zeros_are_present(self):
         with pytest.raises(ValueError):
-            index_of_last_initial_zero([1,2,3,4])
+            index_of_last_initial_zero([1, 2, 3, 4])
 
     def test_when_array_contains_strings(self):
         with pytest.raises(ValueError):
-            index_of_last_initial_zero(['0','1','2'])
+            index_of_last_initial_zero(["0", "1", "2"])
