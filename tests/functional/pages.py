634bbb4c6e402c02fdda81014bafa12540af05cb
Frankie Roberto <frankie@frankieroberto.com>
2018-10-18 16:30:46 +0100

    Simplify auto-complete filling in
    This seems to work locally...

--- tests/functional/pages.py
+++ tests/functional/pages.py
@@ -433,12 +433,12 @@ class MeasureEditPage(BasePage):
         body = self.driver.find_element_by_tag_name("body")
         element = self.wait_for_element(locator)
 
-        body.send_keys(Keys.CONTROL + Keys.HOME)
-        actions = ActionChains(self.driver)
-        actions.move_to_element(element)
-        actions.send_keys_to_element(body, 8 * Keys.ARROW_UP)
-        actions.move_to_element(element)
-        actions.perform()
+        # body.send_keys(Keys.CONTROL + Keys.HOME)
+        # actions = ActionChains(self.driver)
+        # actions.move_to_element(element)
+        # actions.send_keys_to_element(body, 8 * Keys.ARROW_UP)
+        # actions.move_to_element(element)
+        # actions.perform()
 
         element.clear()
         element.send_keys(value)
