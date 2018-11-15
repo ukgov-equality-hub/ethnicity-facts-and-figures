67c43328f265dd91c1e74b7a2d83b493d5db0588
Frankie Roberto <frankie@frankieroberto.com>
2018-11-15 14:28:34 +0000

    Disallow nulls in dimension timestamps (#851)
    This updates the database to no longer allow null values for the `created_at` and `updated_at` timestamps for the dimension rows.
    
    This can only be run after the `2018-12-12_backfill_dimension_timestamps` script has run to backfill the existing null values.

---
+++ migrations/versions/2018_11_14_ban_dimension_timestamp_nulls.py
@@ -0,0 +1,27 @@
+"""ban_dimension_timestamp_nulls
+
+Revision ID: aaaff196cfda
+Revises: 2018_11_12_dimension_timestamps
+Create Date: 2018-11-14 11:44:25.071102
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = 'ban_dimension_timestamp_nulls'
+down_revision = '2018_11_12_dimension_timestamps'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('dimension', 'created_at', nullable=False)
+    op.alter_column('dimension', 'updated_at', nullable=False)
+
+
+def downgrade():
+    op.alter_column('dimension', 'updated_at', nullable=True)
+    op.alter_column('dimension', 'created_at', nullable=True)
+
