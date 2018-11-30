bb8abfff6eea292f90636f7911b7ba76c8a4b947
Samuel Williams <samuelhwilliams@gmail.com>
2018-11-30 22:57:09 +0000

    Add default naming convention for sqlalchemy

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,16 @@
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+db = SQLAlchemy(
+    metadata=MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_N_label)s",
+            "uq": "uix_%(table_name)s_%(column_0_N_name)s",
+            "ck": "ck_%(constraint_name)s",
+            "fk": "%(table_name)s_%(column_0_N_name)s_fkey",
+            "pk": "%(table_name)s_pkey",
+        }
+    )
+)
 mail = Mail()
