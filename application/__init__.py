2d37512fd96360be1acb4f15647d05615e19056b
Samuel Williams <samuelhwilliams@gmail.com>
2018-12-03 10:58:13 +0000

    Add ideal database naming convention
     ## Summary
    Add a naming convention so that sqlalchemy can consistently derive names
    for constraints on the database. This would ensure  migrations are cleanly
    and reproducibly derived.

--- application/__init__.py
+++ application/__init__.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
 db = SQLAlchemy(
+    # Our ideal naming convention
     metadata=MetaData(
         naming_convention={
-            "ix": "ix_%(column_0_N_label)s",
-            "uq": "uix_%(table_name)s_%(column_0_N_name)s",
+            "ix": "ix_%(column_0N_label)s",
+            "uq": "uix_%(table_name)s_%(column_0_name)s",
             "ck": "ck_%(constraint_name)s",
-            "fk": "%(table_name)s_%(column_0_N_name)s_fkey",
+            "fk": "%(table_name)s_%(column_0_name)s_fkey",
             "pk": "%(table_name)s_pkey",
         }
     )
