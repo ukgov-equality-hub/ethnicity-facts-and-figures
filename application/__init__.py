0289d507b1d34f63e43ff60f46e6cec5d06171f0
Samuel Williams <samuelhwilliams@gmail.com>
2018-12-05 18:31:26 +0000

    Update ideal naming convention
     ## Summary
    `uq` seems to be more conventional for unique constraints

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy(
     metadata=MetaData(
         naming_convention={
             "ix": "ix_%(column_0N_label)s",
-            "uq": "uix_%(table_name)s_%(column_0_name)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
             "ck": "ck_%(constraint_name)s",
             "fk": "%(table_name)s_%(column_0_name)s_fkey",
             "pk": "%(table_name)s_pkey",
