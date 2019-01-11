5ce31e046f87e719d49afa9411cf60cfd9a03a9b
Samuel Williams <samuelhwilliams@gmail.com>
2019-01-11 11:44:09 +0000

    Update test to not require fixtures

--- tests/application/cms/test_page_hierarchy_models.py
+++ tests/application/cms/test_page_hierarchy_models.py
@@ -1,25 +1,39 @@
+from application.cms.models import Subtopic, Measure
+
+
 def test_references_between_subtopic_and_topic(stub_topic, stub_subtopic):
     assert stub_topic.subtopics == [stub_subtopic]
     assert stub_subtopic.topic == stub_topic
 
 
-def test_reference_from_subtopic_to_measures(stub_subtopic, stub_measure_1, stub_measure_2):
+def test_reference_from_subtopic_to_measures():
     # Given a subtopic with no measures
-    assert stub_subtopic.measures == []
+    subtopic = Subtopic()
+    assert subtopic.measures == []
+
     # When a subtopic is added to a measure
-    stub_measure_1.subtopics.append(stub_subtopic)
+    measure_1 = Measure()
+    measure_1.subtopics.append(subtopic)
+
     # Then the subtopic has that measure in its measures list
-    assert stub_subtopic.measures == [stub_measure_1]
+    assert subtopic.measures == [measure_1]
+
     # When another measure has the same subtopic
-    stub_measure_2.subtopics.append(stub_subtopic)
+    measure_2 = Measure()
+    measure_2.subtopics.append(subtopic)
+
     # Then both measures are in the measures list
-    assert stub_subtopic.measures == [stub_measure_1, stub_measure_2]
+    assert subtopic.measures == [measure_1, measure_2]
 
 
-def test_reference_from_measure_to_subtopics(stub_subtopic, stub_measure_1, stub_measure_2):
+def test_reference_from_measure_to_subtopics():
     # Given a measure with no subtopic
-    assert stub_measure_1.subtopics == []
+    measure = Measure()
+    assert measure.subtopics == []
+
     # When a measure is added to a subtopic
-    stub_subtopic.measures.append(stub_measure_1)
+    subtopic = Subtopic()
+    subtopic.measures.append(measure)
+
     # Then the measure has that subtopic in its subtopics list
-    assert stub_measure_1.subtopics == [stub_subtopic]
+    assert measure.subtopics == [subtopic]
