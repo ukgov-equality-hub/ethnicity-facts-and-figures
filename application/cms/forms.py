6a2b131d9a7429167bb6380f5eb7ad02964c78b5
Frankie Roberto <frankie@frankieroberto.com>
2019-02-15 17:11:00 +0000

    remove comma

--- application/cms/forms.py
+++ application/cms/forms.py
@@ -200,7 +200,7 @@ class MeasureVersionForm(FlaskForm):
     description = RDUTextAreaField(
         label="Short summary",
         validators=[RequiredForReviewValidator()],
-        hint="This may appear in search results. Aim for less than 160 characters, and end with a full stop.",
+        hint="This may appear in search results. Aim for less than 160 characters and end with a full stop.",
         strip_whitespace=True,
     )
 
