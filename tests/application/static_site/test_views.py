7f9fef80076cc0be905290f92a22e4fffb0e762f
Samuel Williams <samuelhwilliams@gmail.com>
2019-03-06 12:33:14 +0000

    Use `latest` version reference instead of number
    When we publish measure versions, we publish the latest minor update to
    each major version under it's numerical identifier (e.g. 1.2), with the
    exception of the *very* latest version, which is only available under
    `latest`. Therefore, when creating links from the corrections page, we
    need to use the `latest` identifier if there are no other versions which
    have been published afterwards in order to get a valid URL.
    
    An alternative solution is to duplicate the latest published version and
    allow it to be referenced both by its numerical id and the word
    `latest`, but this felt quicker to get out and less likely to lead to
    dead links hanging around in the future.
    
    https://trello.com/c/3zkveCER/1383

--- tests/application/static_site/test_views.py
+++ tests/application/static_site/test_views.py
@@ -1,3 +1,4 @@
+import datetime
 import re
 
 import pytest
@@ -850,3 +851,66 @@ class TestMeasurePage:
         data_links = page.findAll("a", href=True, text=re.compile(r"Test measure page data\s+-\s+Spreadsheet"))
         assert len(data_links) == 1
         assert data_links[0].attrs["href"] == expected_url
+
+
+class TestCorrections:
+    def setup(self):
+        self.measure_version_1 = MeasureVersionFactory.create(
+            version="1.0",
+            status="APPROVED",
+            update_corrects_data_mistake=False,
+            title="Measure 1 version 1.0",
+            published_at=datetime.datetime(2000, 1, 1),
+        )
+        MeasureVersionFactory.create(
+            version="1.1",
+            status="APPROVED",
+            update_corrects_data_mistake=True,
+            title="Measure 1 version 1.1",
+            published_at=datetime.datetime(2001, 1, 1),
+            measure=self.measure_version_1.measure,
+        )
+
+        self.measure_version_2 = MeasureVersionFactory.create(
+            version="1.0",
+            status="APPROVED",
+            update_corrects_data_mistake=False,
+            title="Measure 2 version 1.0",
+            published_at=datetime.datetime(2002, 1, 1),
+        )
+        MeasureVersionFactory.create(
+            version="1.1",
+            status="APPROVED",
+            update_corrects_data_mistake=True,
+            title="Measure 2 version 1.1",
+            published_at=datetime.datetime(2003, 1, 1),
+            measure=self.measure_version_2.measure,
+        )
+        MeasureVersionFactory.create(
+            version="1.2",
+            status="APPROVED",
+            update_corrects_data_mistake=False,
+            title="Measure 2 version 1.2",
+            published_at=datetime.datetime(2004, 1, 1),
+            measure=self.measure_version_2.measure,
+        )
+
+    def test_corrections_listed_in_reverse_chronological_order(self, test_app_client, logged_in_rdu_user):
+        resp = test_app_client.get("/corrections", follow_redirects=False)
+        page = BeautifulSoup(resp.data.decode("utf-8"), "html.parser")
+
+        corrected_measure_versions = page.findAll("div", {"class": "corrected-measure-version"})
+
+        assert corrected_measure_versions[0].find("a").text.strip() == "Measure 2 version 1.1"
+        assert corrected_measure_versions[1].find("a").text.strip() == "Measure 1 version 1.1"
+
+    def test_latest_published_version_uses_latest_reference_rather_than_numerical_version_if_no_later_versions(
+        self, test_app_client, logged_in_rdu_user
+    ):
+        resp = test_app_client.get("/corrections", follow_redirects=False)
+        page = BeautifulSoup(resp.data.decode("utf-8"), "html.parser")
+
+        corrected_measure_versions = page.findAll("div", {"class": "corrected-measure-version"})
+
+        assert corrected_measure_versions[0].find("a").attrs["href"].endswith("1.2")
+        assert corrected_measure_versions[1].find("a").attrs["href"].endswith("latest")
