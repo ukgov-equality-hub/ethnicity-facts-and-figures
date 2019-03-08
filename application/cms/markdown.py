d70314fa9d6ae383c1086442ee3c292433c60a95
Samuel Williams <samuelhwilliams@gmail.com>
2019-03-08 14:50:20 +0000

    Add Design System Markdown processor
    Add a custom markdown treeprocessor that injects GOV.UK Design System
    classes onto our core elements (p, a, ul, ol) so that markdown entered
    by CMS users still renders nicely when we pump it into our templates.
    
    We should no longer import directly from the Python `markdown` library.
    Instead, we should import the interface exposed in
    `application.cms.markdown`, which handles automatically registering our
    extension. Fortunately, it looks like all of our current markdown
    rendering happens through a `render_markdown` filter, so this is nicely
    handled for us already.

---
+++ application/cms/markdown.py
@@ -0,0 +1,32 @@
+from markdown import Markdown
+from markdown.extensions import Extension
+from markdown.treeprocessors import Treeprocessor
+
+
+class DesignSystemMarkdownProcessor(Treeprocessor):
+    def run(self, root):
+        for p in root.iter("p"):
+            p.set("class", "govuk-body")
+
+        for a in root.iter("a"):
+            a.set("class", "govuk-link")
+
+        for ul in root.iter("ul"):
+            ul.set("class", "govuk-list govuk-list--bullet")
+
+        for ol in root.iter("ol"):
+            ol.set("class", "govuk-list govuk-list--number")
+
+
+class DesignSystemMarkdownExtension(Extension):
+    def extendMarkdown(self, md):
+        md.treeprocessors.register(
+            item=DesignSystemMarkdownProcessor(md),
+            name="design-system-markdown",
+            priority=175,  # Arbitrary priority copied from Python Markdown docs
+        )
+
+
+def markdown(text):
+    md = Markdown(extensions=[DesignSystemMarkdownExtension()])
+    return md.convert(text)
