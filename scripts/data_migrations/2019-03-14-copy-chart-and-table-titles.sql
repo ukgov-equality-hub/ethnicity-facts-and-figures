3b2d980a178a064ebbe8be9d2eddc9d53d1dff8f
Frankie Roberto <frankie@frankieroberto.com>
2019-03-14 16:06:14 +0000

    Fix data migration

--- scripts/data_migrations/2019-03-14-copy-chart-and-table-titles.sql
+++ scripts/data_migrations/2019-03-14-copy-chart-and-table-titles.sql
@@ -1,5 +1,5 @@
 -- This copies the chart and table titles form the settings object into its own column.
 
-update dimension_chart set title = settings_and_source_data::json->'chartFormat'->'chart_title';
+update dimension_chart set title = settings_and_source_data::json->'chartFormat'->>'chart_title'::text;
 
-update dimension_table set title = settings_and_source_data::json->'tableValues'->'table_title';
+update dimension_table set title = settings_and_source_data::json->'tableValues'->>'table_title'::text;
