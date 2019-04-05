cf8d775dc045d49b4b60aaea8e749cefaf5d883f
Samuel Williams <samuelhwilliams@gmail.com>
2019-04-05 09:00:00 +0100

    Prevent 500 on login attempt with bad password
    Fix a bug that caused 500s when logging in with the wrong password and
    add a test that validates the expected behaviour (page returns a 200 and
    displays error summary messages asking the user to check their email
    address and password).

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,4 @@
+from lxml import html
 from application.cms.models import MeasureVersion
 
 
@@ -19,3 +20,13 @@ def get_page_with_title(title):
 
 def assert_strings_match_ignoring_whitespace(string_1, string_2):
     assert "".join(string_1.split()) == "".join(string_2.split())
+
+
+def page_displays_error_matching_message(response, message: str) -> bool:
+    doc = html.fromstring(response.get_data(as_text=True))
+
+    error_summary = doc.xpath(
+        "//div[contains(@class, 'govuk-error-summary')]//a[normalize-space(text())=$message]", message=message
+    )
+
+    return True if error_summary else False
