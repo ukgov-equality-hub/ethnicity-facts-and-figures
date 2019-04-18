5bee32e8721dfe97511269d846ef0185a558ca05
Frankie Roberto <frankie@frankieroberto.com>
2019-04-18 12:03:32 +0100

    New title and description

---
+++ scripts/data_migrations/2019-04-18_update_british_population_topic.sql
@@ -0,0 +1,4 @@
+UPDATE topic
+SET title = 'UK population by ethnicity', additional_description = '
+In the 2011 Census, 86% of people in England and Wales said they were White, and 14% were from ethnic minorities (choosing from this [list of ethnic groups](/ethnicity-in-the-uk/ethnic-groups-and-data-collected)).'
+WHERE slug = 'british-population';
