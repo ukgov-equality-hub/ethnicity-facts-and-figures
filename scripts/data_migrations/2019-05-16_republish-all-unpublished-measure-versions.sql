fb169d0397f0713b8c9c2d5730b00e2f97c802e6
Samuel Williams <samuelhwilliams@users.noreply.github.com>
2019-05-17 09:17:38 +0100

    Remove build record

--- scripts/data_migrations/2019-05-16_republish-all-unpublished-measure-versions.sql
+++ scripts/data_migrations/2019-05-16_republish-all-unpublished-measure-versions.sql
@@ -5,10 +5,3 @@ SET
 WHERE
     status = 'UNPUBLISH'
     or status = 'UNPUBLISHED';
-
-INSERT
-INTO
-    build
-    (id, created_at, status)
-VALUES
-    ('6bd13007-9e8e-4f53-bdcc-fdd3f8728611', now(), 'PENDING');
