c255e1e869a6e00065545b71b394befe994071ef
Frankie Roberto <frankie@frankieroberto.com>
2019-07-11 10:35:20 +0100

    be even more explicit
    Co-Authored-By: Samuel Williams <samuelhwilliams@users.noreply.github.com>

--- application/cms/filters.py
+++ application/cms/filters.py
@@ -77,4 +77,4 @@ def html_line_breaks(string):
 
 
 def url_with_line_breaks(string):
-    return string.replace("/", "/​")  # Adds a zero-width space after the /
+    return string.replace("/", "/\N{ZERO WIDTH SPACE}")
