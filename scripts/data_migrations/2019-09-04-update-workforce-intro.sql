a6bbffb7fcf74f9d8c1d777faf54d084809afa0e
Frankie Roberto <frankie@frankieroberto.com>
2019-09-04 15:22:58 +0100

    Link to report from within topic description

---
+++ scripts/data_migrations/2019-09-04-update-workforce-intro.sql
@@ -0,0 +1,3 @@
+UPDATE topic SET additional_description = 'The Race Disparity Unit have published the first report in an annual series summarising diversity in the public sector: <a href="https://www.gov.uk/government/publications/ethnic-diversity-of-public-sector-workforces">Ethnic diversity of public sector workforces 2019</a>.
+
+Find data on workforce ethnic diversity, pay disparities and small businesses.' WHERE slug = 'workforce-and-business';
