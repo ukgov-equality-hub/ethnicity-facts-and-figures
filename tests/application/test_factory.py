6df85e422eed60e463eaa1f91b6b0d2282621c41
Mehmet Duran <mehmet.duran@gmail.com>
2019-09-16 15:28:20 +0100

    Added tests for headers

--- tests/application/test_factory.py
+++ tests/application/test_factory.py
@@ -1,4 +1,5 @@
 from flask import render_template_string
+from werkzeug.test import EnvironBuilder, run_wsgi_app
 
 
 class TestTemplateGlobals:
@@ -17,3 +18,16 @@ class TestTemplateGlobals:
 
         assert client.get("/test-globals?static_mode=no").get_data(as_text=True) == "False"
         assert client.get("/test-globals?static_mode=yes").get_data(as_text=True) == "True"
+
+
+class TestHeaders:
+    def test_http_headers(self, single_use_app):
+        builder = EnvironBuilder(path="/", method="GET")
+        env = builder.get_environ()
+
+        (app_iter, status, headers) = run_wsgi_app(single_use_app, env)
+        assert headers.get("X-Frame-Options") == "deny"
+        assert headers.get("X-Content-Type-Options") == "nosniff"
+        assert headers.get("X-XSS-Protection") == "1; mode=block"
+        assert headers.get("X-Permitted-Cross-Domain-Policies") == "none"
+        assert headers.get("Content-Security-Policy")
