8a489772158d40c9cff844f2318af4173d7bf848
Panos Paralakis <p.paralakis@gmail.com>
2020-05-27 17:04:09 +0100

    Fix failing pointles test

--- tests/functional/test_search.py
+++ tests/functional/test_search.py
@@ -1,15 +1,6 @@
 from tests.functional.pages import HomePage
 
 
-def test_site_search_form_action_updated_by_javascript(driver, live_server):
-    home_page = HomePage(driver, live_server)
-    home_page.get()
-    assert home_page.is_current()
-
-    expected_url = f"http://localhost:{live_server.port}/search"
-    assert driver.find_element_by_id("search-form").get_attribute("action") == expected_url
-
-
 def test_can_search_from_homepage_with_javascript(driver, live_server):
     home_page = HomePage(driver, live_server)
     home_page.get()
