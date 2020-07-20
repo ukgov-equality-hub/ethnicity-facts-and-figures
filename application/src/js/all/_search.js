5af7e58a7a33671e10998559fac7cad2c8977ccb
Panos Paralakis <p.paralakis@gmail.com>
2020-07-20 11:17:08 +0100

    Update homepage layout

--- application/src/js/all/_search.js
+++ application/src/js/all/_search.js
@@ -6,7 +6,7 @@
                 /* The default form action is Google's custom search URL, which takes us off site.
                     We want to keep users on the site if possible, so we have own our search page for results. */
                 document.getElementById('search-form').action = '/search';
-                document.getElementById('site-search-label').innerHTML = 'Search facts and figures';
+                // document.getElementById('site-search-label').innerHTML = 'Search facts and figures';
 
                 /* We add a form field to direct users to our own custom search engine when javascript isn't
                     available. If it is, we keep them on our site, and so we don't want the custom search ID to
