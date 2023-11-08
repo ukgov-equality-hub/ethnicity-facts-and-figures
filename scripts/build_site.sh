736a2a46a67936e2cee1f591a621c061b2164cd4
James Griffiths <james@jwgsoftware.com>
2023-11-08 14:35:06 +0000

    EHD-503: Setup EFF hosting using Terraform: Changes required to build the static site on Elastic Beanstalk

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python3 /home/eff/ethnicity-facts-and-figures-publisher/manage.py force_build_static_site
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+python3 "${SCRIPT_DIR}/../manage.py" force_build_static_site > /var/log/static-site-build.log 2>&1 &
