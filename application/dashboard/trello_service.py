72c684b463dd44f794c109470824d9f65a88350d
James Griffiths <james@jwgsoftware.com>
2024-03-27 14:48:47 +0000

    EHD-1034: Fix Trello API client: Trello API no longer allows GET requests with a body. API client hasn't been updated, so we need to patch it ourselves

--- application/dashboard/trello_service.py
+++ application/dashboard/trello_service.py
@@ -1,5 +1,6 @@
 import re
-
+import json
+import trello
 from trello import TrelloClient
 
 from application.cms.service import Service
@@ -58,6 +59,64 @@ ORGANISATION_LABELS = {
     "MoJ": "Ministry of Justice",
     "ONS": "Office for National Statistics",
 }
+
+
+# "What is this method doing here?", you may ask:
+# Trello have made this change: https://developer.atlassian.com/changelog/#CHANGE-1459 ("Trello API will no longer accept GET request with data in body")
+# The Trello API client we're using (py-trello) hasn't been updated to work with this change
+# An issue has been raised, but not yet fixed, but there is a suggested work-around: https://github.com/sarumont/py-trello/issues/373#issuecomment-1958814294
+def patched_fetch_json(self,
+                       uri_path,
+                       http_method='GET',
+                       headers=None,
+                       query_params=None,
+                       post_args=None,
+                       files=None):
+    """ Fetch some JSON from Trello """
+
+    # explicit values here to avoid mutable default values
+    if headers is None:
+        headers = {}
+    if query_params is None:
+        query_params = {}
+    if post_args is None:
+        post_args = {}
+
+    # if files specified, we don't want any data
+    data = None
+    if files is None and post_args != {}:
+        data = json.dumps(post_args)
+
+    # set content type and accept headers to handle JSON
+    if http_method in ("POST", "PUT", "DELETE") and not files:
+        headers['Content-Type'] = 'application/json; charset=utf-8'
+
+    headers['Accept'] = 'application/json'
+
+    # construct the full URL without query parameters
+    if uri_path[0] == '/':
+        uri_path = uri_path[1:]
+    url = 'https://api.trello.com/1/%s' % uri_path
+
+    if self.oauth is None:
+        query_params['key'] = self.api_key
+        query_params['token'] = self.api_secret
+
+    # perform the HTTP requests, if possible uses OAuth authentication
+    response = self.http_service.request(http_method, url, params=query_params,
+                                         headers=headers, data=data,
+                                         auth=self.oauth, files=files,
+                                         proxies=self.proxies)
+
+    if response.status_code == 401:
+        raise trello.Unauthorized("%s at %s" % (response.text, url), response)
+    if response.status_code != 200:
+        raise trello.ResourceUnavailable("%s at %s" % (response.text, url), response)
+
+    return response.json()
+
+
+trello.TrelloClient.fetch_json = patched_fetch_json
 
 
 class TrelloService(Service):
